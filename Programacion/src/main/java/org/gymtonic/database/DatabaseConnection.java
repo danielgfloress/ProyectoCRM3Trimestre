2ce22b0de723e80688fdba73852eb045a5ba7495
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-13 09:48:02 +0200

    Database joined with Java

--- Programacion/src/main/java/org/gymtonic/database/DatabaseConnection.java
+++ Programacion/src/main/java/org/gymtonic/database/DatabaseConnection.java
@@ -1,4 +1,26 @@
 package org.gymtonic.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private final static String URL = "jdbc:mysql://localhost:3306/crm_gimnasio";
+    private final static String USER = "root";
+    private final static String PASSWORD = "1234";
+    private static Connection connection;
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+
+        if (connection == null) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+
+        return connection;
+
+    }
+
 }
