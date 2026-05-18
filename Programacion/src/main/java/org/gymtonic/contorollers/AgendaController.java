005e58aa3e352443249f5fccd5d5df81a66c3461
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-18 08:49:30 +0200

    AgendaController finished

--- Programacion/src/main/java/org/gymtonic/contorollers/AgendaController.java
+++ Programacion/src/main/java/org/gymtonic/contorollers/AgendaController.java
@@ -1,4 +1,32 @@
 package org.gymtonic.contorollers;
 
+import org.gymtonic.models.Agenda;
+import org.gymtonic.service.AgendaService;
+
+import java.util.List;
+
 public class AgendaController {
+
+    private final AgendaService agendaService = new AgendaService();
+
+    public List<Agenda> findAll() {
+        return agendaService.findAll();
+    }
+
+    public Agenda findById(Long id) {
+        return agendaService.findById(id);
+    }
+
+    public void addAgenda(Agenda agenda) {
+        agendaService.addAgenda(agenda);
+    }
+
+    public boolean deleteAgenda(Long id) {
+        return agendaService.deleteAgenda(id);
+    }
+
+    public boolean modifyAgenda(Long id, Agenda agenda) {
+        return agendaService.modifyAgenda(id, agenda);
+    }
+
 }
