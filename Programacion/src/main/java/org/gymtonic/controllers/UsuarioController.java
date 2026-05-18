13382ea31d5944c86e756ef23ad138042a6e4acc
daniel <danielgarciaf07@gmail.com>
2026-05-18 14:25:46 +0200

    Adding the controller of usuario

--- Programacion/src/main/java/org/gymtonic/controllers/UsuarioController.java
+++ Programacion/src/main/java/org/gymtonic/controllers/UsuarioController.java
@@ -1,4 +1,33 @@
 package org.gymtonic.controllers;
 
+import org.gymtonic.models.Usuario;
+import org.gymtonic.service.UsuarioService;
+
+import java.util.List;
+
 public class UsuarioController {
+
+    private final UsuarioService usuarioService = new UsuarioService();
+
+    public List<Usuario> findAll() {
+        return usuarioService.findAll();
+    }
+
+    public Usuario findById(Long id) {
+        return usuarioService.findById(id);
+    }
+
+    public void addUsuario(Usuario usuario) {
+        usuarioService.addUsuario(usuario);
+    }
+
+    public boolean deleteUsuario(Long id) {
+        return usuarioService.deleteUsuario(id);
+    }
+
+    public boolean modifyUsuario(Long id, Usuario usuario) {
+        return usuarioService.modifyUsuario(id, usuario);
+    }
+
 }
+
