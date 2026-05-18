3cf5d82cd21630e3b516c063d8616cb86931f0d3
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-18 08:58:46 +0200

    ClienteController done

--- Programacion/src/main/java/org/gymtonic/contorollers/ClienteController.java
+++ Programacion/src/main/java/org/gymtonic/contorollers/ClienteController.java
@@ -1,4 +1,32 @@
 package org.gymtonic.contorollers;
 
+import org.gymtonic.models.Cliente;
+import org.gymtonic.service.ClienteService;
+
+import java.util.List;
+
 public class ClienteController {
+
+    private final ClienteService clienteService = new ClienteService();
+
+    public List<Cliente> findAll() {
+        return clienteService.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        return clienteService.findById(id);
+    }
+
+    public void addCliente(Cliente cliente) {
+        clienteService.addCliente(cliente);
+    }
+
+    public boolean deleteCliente(Long id) {
+        return clienteService.deleteCliente(id);
+    }
+
+    public boolean modifyCliente(Long id, Cliente cliente) {
+        return clienteService.modifyCliente(id, cliente);
+    }
+
 }
