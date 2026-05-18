a4cab1400fd614c59f241b87ab917be88ee39bad
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-18 10:14:52 +0200

    begin ClaseMenu

--- Programacion/src/main/java/org/gymtonic/menus/ClaseMenu.java
+++ Programacion/src/main/java/org/gymtonic/menus/ClaseMenu.java
@@ -1,4 +1,38 @@
 package org.gymtonic.menus;
 
+import org.gymtonic.controllers.ClaseController;
+
+import java.util.Scanner;
+
+
 public class ClaseMenu {
+
+    private final Scanner sc;
+    private final ClaseController claseController = new ClaseController();
+
+    public ClaseMenu(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void mostrar() {
+
+        int opcion;
+
+        do {
+            System.out.println("\n--- CLASES ---");
+            System.out.println("1. Listar todas");
+            System.out.println("2. Buscar por ID");
+            System.out.println("3. Añadir clase");
+            System.out.println("4. Modificar clase");
+            System.out.println("5. Eliminar clase");
+            System.out.println("6. Volver");
+            System.out.print("Selecciona una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+            }
+
+        } while (opcion != 6);
+
+    }
 }
