b8172833716fe0288270f80df198cbc94898ce71
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-18 08:57:05 +0200

    ClaseController done

--- Programacion/src/main/java/org/gymtonic/contorollers/ClaseController.java
+++ Programacion/src/main/java/org/gymtonic/contorollers/ClaseController.java
@@ -1,4 +1,32 @@
 package org.gymtonic.contorollers;
 
+import org.gymtonic.models.Clase;
+import org.gymtonic.service.ClaseService;
+
+import java.util.List;
+
 public class ClaseController {
+
+    private final ClaseService claseService = new ClaseService();
+
+    public List<Clase> findAll() {
+        return claseService.findAll();
+    }
+
+    public Clase findById(Long id) {
+        return claseService.findById(id);
+    }
+
+    public void addClase(Clase clase) {
+        claseService.addClase(clase);
+    }
+
+    public boolean deleteClase(Long id) {
+        return claseService.deleteClase(id);
+    }
+
+    public boolean modifyClase(Long id, Clase clase) {
+        return claseService.modifyClase(id, clase);
+    }
+
 }
