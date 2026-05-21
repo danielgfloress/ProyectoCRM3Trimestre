dd364a8d39c3fa894fd29d500940fdb21aec42b1
cmunozcad-sys <cmunozcad@gmail.com>
2026-05-21 08:48:40 +0200

    Main do it

--- Programacion/src/main/java/org/gymtonic/Main.java
+++ Programacion/src/main/java/org/gymtonic/Main.java
@@ -1,7 +1,13 @@
 package org.gymtonic;
 
+import org.gymtonic.menus.Menu;
+
 public class Main {
     public static void main(String[] args) {
+
+        Menu menu = new Menu();
+
+        menu.menu();
 
     }
 }
